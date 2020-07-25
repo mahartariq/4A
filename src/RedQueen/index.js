8fe1cf355993d2d566253928c233a5de10aa0899
MaharTariq <mahartariq499@gmail.com>
2020-07-25 20:44:47 +0500

    lets hope

--- src/RedQueen/index.js
+++ src/RedQueen/index.js
@@ -11,23 +11,26 @@ export const RedQueenRace = () => {
     </div>
   </div>
 
+  <div className="scenery">
   <div id="foreground1">
-    <img id="palm3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"  alt="Alice and the Red Queen running to stay in place."/>
+    <img id="palm3" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"} srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"  alt="Alice and the Red Queen running to stay in place."/>
   </div>
+  </div>
+
   <div className="scenery" id="foreground2">    
-    <img id="bush" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x" alt="ta"/>
-    <img id="w_rook_upright" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x" alt="ad"/>
+    <img id="bush" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"} srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x" alt="ta"/>
+    <img id="w_rook_upright" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png"} srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x" alt="ad"/>
   </div>
   <div className="scenery" id="background1">
-    <img id="r_pawn_upright" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x" alt="adf"/>
-    <img id="w_rook" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x" alt="adfc"/>
-    <img id="palm1" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x" alt="eta"/>
+    <img id="r_pawn_upright" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"} srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x" alt="adf"/>
+    <img id="w_rook" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"} srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x" alt="adfc"/>
+    <img id="palm1" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"} srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x" alt="eta"/>
   </div>
   <div className="scenery" id="background2">
-    <img id="r_pawn" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x" alt/>
+    <img id="r_pawn" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png"} srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x" alt/>
 
-    <img id="r_knight" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x" alt/>
-    <img id="palm2" alt src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"/>
+    <img id="r_knight" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png"} srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x" alt/>
+    <img id="palm2" alt src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png"} srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"/>
   </div>
 </div>
         </div>
